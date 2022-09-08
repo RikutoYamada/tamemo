500d08272d2ca2df8b7f353f5a05fa132d9692dc
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-09-08 23:38:08 +0900

    Edit /users/sessions_controller.rb and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get '/users/sign_in', to: 'users/sessions#new'
+    get '/users/sign_out', to: 'users/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

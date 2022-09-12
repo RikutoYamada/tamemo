12b0b48973ff4a63a143329238b9d712d5ecd634
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-09-13 07:46:07 +0900

    Japanese language support

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,8 @@ module Tamemo
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    #Japanese language support
+    config.i18n.default_locale = :ja
   end
 end

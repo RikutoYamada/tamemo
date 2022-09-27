c2cbc82265fb38023cbb115bd6eb7b8c7f050744
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-09-27 22:49:44 +0900

    rubocop -A spec/models/user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is valid with name, email and password" do
-    user = User.new(
-    name: "yamada",
-    email: "test@example.com",
-    password: "password"
+    user = described_class.new(
+      name: "yamada",
+      email: "test@example.com",
+      password: "password"
     )
     expect(user).to be_valid
   end

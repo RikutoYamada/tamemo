2be7192c8200bf51186d212423fa366754bb3482
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-09-27 21:53:54 +0900

    Edit user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with name, email and password" do
+    user = User.new(
+    name: "yamada",
+    email: "test@example.com",
+    password: "password"
+    )
+    expect(user).to be_valid
+  end
 end

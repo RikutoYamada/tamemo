91ad90c68383a95b1cfee495491e31eacc4c0975
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-07 22:56:43 +0900

    make association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :expense_categories, dependent: :destroy
+  has_many :expenses, dependent: :destroy
 end

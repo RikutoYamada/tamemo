8017ab530d9f45c3951f20138aab8b281fbd18cd
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-09 21:03:12 +0900

    Changed expense page to show only large categories

--- app/controllers/expense_categories_controller.rb
+++ app/controllers/expense_categories_controller.rb
@@ -1,5 +1,10 @@
 class ExpenseCategoriesController < ApplicationController
   def index
+    @categories = if params[:is_super]
+                    User.first.expense_categories.select(&:root?)
+                  else
+                    User.first.expense_categories.reject(&:root?)
+                  end
     @user = current_user
   end
 end

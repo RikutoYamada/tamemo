d6a4f366a2cbde74abf9219158a381c2a0928b8e
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-09 15:29:27 +0900

    Displays expenditure history on home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+rikuto = User.create(name: "rikuto", email: "rikuto@gmail.com", password: "rikuto")
+mei = User.create(name: "mei", email: "mei@gmail.com", password: "satomei")
+food_expense = rikuto.expense_categories.create(name: "食費")
+transportation_expense = rikuto.expense_categories.create(name: "交通費")
+
+%w[食料品 外食費 飲み代].each do |name|
+  food_expense.children.create(user_id: 1, name: name)
+end
+
+%w[電車賃 タクシー代 ガソリン代].each do |name|
+  transportation_expense.children.create(user_id: 2, name: name)
+end
+
+rikuto.expenses.create(category_id: 3, store: "コンビニ", amount: 500, expended_at: Date.today)
+rikuto.expenses.create(category_id: 5, store: "鳥貴族", amount: 3000, expended_at: Date.today)
+mei.expenses.create(category_id: 8, store: "コスモ石油", amount: 2500, expended_at: Date.today)

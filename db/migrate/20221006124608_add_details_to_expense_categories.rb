c4a35f93d26e02d09e0642afc3e7e6f095409c18
RikutoYamada <88074385+RikutoYamada@users.noreply.github.com>
2022-10-15 19:19:38 +0900

    Delete unnecessary migration files

--- db/migrate/20221006124608_add_details_to_expense_categories.rb
+++
@@ -1,5 +0,0 @@
-class AddDetailsToExpenseCategories < ActiveRecord::Migration[7.0]
-  def change
-    add_column :expense_categories, :is_enabled, :boolean
-  end
-end
--- db/migrate/20221007122440_remove_is_enabled_from_expense_categories.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIsEnabledFromExpenseCategories < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :expense_categories, :is_enabled, :string
-  end
-end
